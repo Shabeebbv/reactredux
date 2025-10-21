befe8e78d80dc181e440b5bb7ee491486f718718
Mohammed shabeeb <sabishabeeb2@gmail.com>
2025-10-21 20:04:15 +0530

    reactredux

---
+++ store.jsx
@@ -0,0 +1,19 @@
+import { createStore } from "redux"
+
+const initialState={
+    count:0
+}
+function reducer(state=initialState,action){
+    switch(action.type){
+        case "increment":
+            return {...state,count : state.count+1}
+        case "decrement": 
+        return{...state,count:state.count-1}
+        default:
+            return state
+    }
+}
+
+export const store=createStore(reducer)    
+// console.log(store.getState);
+export default reducer
